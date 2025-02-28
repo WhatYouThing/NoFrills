f512c75505fc2467fb295ad0493090df5174270b
WhatYouThing <whatuthing@gmail.com>
2025-02-28 01:20:52 +0100

    fix no pearl cooldown not removing the cooldown properly

--- src/main/java/nofrills/mixin/ItemStackMixin.java
+++ src/main/java/nofrills/mixin/ItemStackMixin.java
@@ -1,12 +1,17 @@
 package nofrills.mixin;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import nofrills.config.Config;
 import nofrills.misc.Utils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import static nofrills.Main.mc;
 
 @Mixin(ItemStack.class)
 public class ItemStackMixin {
@@ -15,6 +20,15 @@ public class ItemStackMixin {
     private void onCapCount(int maxCount, CallbackInfo ci) {
         if (Utils.isFixEnabled(Config.itemCountFix)) {
             ci.cancel();
+        }
+    }
+
+    @Inject(method = "applyRemainderAndCooldown", at = @At("HEAD"), cancellable = true)
+    private void onApplyCooldown(LivingEntity user, ItemStack stack, CallbackInfoReturnable<ItemStack> cir) {
+        if (user.equals(mc.player) && Utils.isFixEnabled(Config.noPearlCooldown)) {
+            if (stack.getItem().equals(Items.ENDER_PEARL)) {
+                cir.setReturnValue(stack);
+            }
         }
     }
 }

00d090f6ca4f14db455ec9e9f644c1a6cfa2c011
WhatYouThing <whatuthing@gmail.com>
2025-04-05 12:18:51 +0200

    add museum support to Recipe Lookup

--- src/main/java/nofrills/features/RecipeLookup.java
+++ src/main/java/nofrills/features/RecipeLookup.java
@@ -5,11 +5,10 @@ import com.google.gson.JsonParser;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.client.gui.screen.ingame.GenericContainerScreen;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
-import net.minecraft.component.DataComponentTypes;
-import net.minecraft.component.type.NbtComponent;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.screen.slot.Slot;
+import net.minecraft.util.Formatting;
 import nofrills.events.InputEvent;
 import nofrills.misc.Utils;
 import nofrills.mixin.HandledScreenAccessor;
@@ -23,25 +22,24 @@ public class RecipeLookup {
         if (Utils.Keybinds.recipeLookup.matchesKey(event.key, 0) && event.action == GLFW.GLFW_PRESS) {
             if (mc.currentScreen instanceof InventoryScreen || mc.currentScreen instanceof GenericContainerScreen) {
                 Slot focused = ((HandledScreenAccessor) mc.currentScreen).getFocusedSlot();
-                if (focused == null) {
-                    return;
-                }
-                ItemStack item = focused.getStack();
-                NbtComponent component = item.get(DataComponentTypes.CUSTOM_DATA);
-                if (!item.isEmpty() && component != null) {
-                    NbtCompound data = component.copyNbt();
-                    String itemId = data.getString("id");
+                if (focused != null) {
+                    ItemStack stack = focused.getStack();
+                    String itemId = Utils.getSkyblockId(stack);
                     if (!itemId.isEmpty()) {
                         if (itemId.contains("GENERATOR")) {
                             int index = itemId.lastIndexOf("_");
                             Utils.sendMessage("/recipe " + itemId.substring(0, index));
                         } else if (itemId.equals("PET")) {
+                            NbtCompound data = Utils.getCustomData(stack);
                             JsonObject petData = JsonParser.parseString(data.getString("petInfo")).getAsJsonObject();
                             String petName = petData.get("type").getAsString().replaceAll("_", " ");
                             Utils.sendMessage("/recipe " + petName + " PET");
                         } else {
                             Utils.sendMessage("/recipe " + itemId);
                         }
+                        event.cancel();
+                    } else if (!stack.isEmpty() && mc.currentScreen.getTitle().getString().startsWith("Museum")) {
+                        Utils.sendMessage("/recipe " + Formatting.strip(stack.getName().getString()));
                         event.cancel();
                     }
                 }
--- src/main/java/nofrills/hud/HudManager.java
+++ src/main/java/nofrills/hud/HudManager.java
@@ -13,8 +13,8 @@ public class HudManager {
     public static SimpleTextElement testElement = new SimpleTextElement(0.25, 0.25, Text.of("§9deez nuts"), RenderColor.fromHex(0xffffff));
     public static SimpleTextElement testElement2 = new SimpleTextElement(0.75, 0.75, Text.of("§6nuts deez"), RenderColor.fromHex(0xffffff));
     public static List<HudElement> elements = List.of(
-            testElement,
-            testElement2
+            //testElement,
+            //testElement2
     );
 
     public static boolean isEditingHud() {

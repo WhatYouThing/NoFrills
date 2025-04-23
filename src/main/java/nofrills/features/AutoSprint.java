3a05e38f4cbbf09672986bf0346098c423dd98ff
WhatYouThing <whatuthing@gmail.com>
2025-04-23 02:45:23 +0200

    remove debug code for entity rendering features

--- src/main/java/nofrills/features/AutoSprint.java
+++ src/main/java/nofrills/features/AutoSprint.java
@@ -1,12 +1,8 @@
 package nofrills.features;
 
 import meteordevelopment.orbit.EventHandler;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
 import nofrills.config.Config;
 import nofrills.events.WorldTickEvent;
-import nofrills.misc.RenderColor;
-import nofrills.misc.Rendering;
 
 import static nofrills.Main.mc;
 
@@ -15,12 +11,6 @@ public class AutoSprint {
     public static void tick(WorldTickEvent event) {
         if (Config.autoSprint) {
             mc.options.sprintKey.setPressed(true);
-        }
-
-        for (Entity ent : mc.world.getEntities()) {
-            if (ent instanceof LivingEntity) {
-                Rendering.Entities.drawOutline(ent, true, RenderColor.fromHex(0xffffff));
-            }
         }
     }
 }

89f0896f03ce92b9fe89bd85ca3b9dfd8d03ac8e
WhatYouThing <whatuthing@gmail.com>
2025-06-15 17:18:43 +0200

    update etherwarp overlay to use the new eye height

--- src/main/java/nofrills/misc/Utils.java
+++ src/main/java/nofrills/misc/Utils.java
@@ -468,10 +468,12 @@ public class Utils {
      * Modified version of Minecraft's raycast function, which considers every block hit as a 1x1 cube, matching how Hypixel performs their raycast for the Ether Transmission ability.
      */
     public static HitResult raycastFullBlock(Entity entity, double maxDistance, float tickDelta) {
-        Vec3d vec3d = entity.getCameraPosVec(tickDelta);
-        Vec3d vec3d2 = entity.getRotationVec(tickDelta);
-        Vec3d vec3d3 = vec3d.add(vec3d2.x * maxDistance, vec3d2.y * maxDistance, vec3d2.z * maxDistance);
-        RaycastContext context = new RaycastContext(vec3d, vec3d3, RaycastContext.ShapeType.OUTLINE, net.minecraft.world.RaycastContext.FluidHandling.ANY, entity);
+        Vec3d height = entity.getLerpedPos(tickDelta).add(0, 1.62, 0); // this is the standing eye height, hoping that is a bug
+        Vec3d camPos = entity.getCameraPosVec(tickDelta);
+        Vec3d rot = entity.getRotationVec(tickDelta);
+        Vec3d pos = new Vec3d(camPos.getX(), height.getY(), camPos.getZ());
+        Vec3d end = pos.add(rot.x * maxDistance, rot.y * maxDistance, rot.z * maxDistance);
+        RaycastContext context = new RaycastContext(pos, end, RaycastContext.ShapeType.OUTLINE, net.minecraft.world.RaycastContext.FluidHandling.ANY, entity);
         ((RaycastOptions) context).nofrills_mod$setConsiderAllFull(true);
         return entity.getWorld().raycast(context);
     }

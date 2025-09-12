31e78a4261dff1ffc2bfca14eb1916391c3956b2
MaxLunar <19411012+MaxLunar@users.noreply.github.com>
2025-09-12 18:46:00 +0300

    More simple and reliable solution

--- src/main/java/nofrills/features/hunting/CinderbatHighlight.java
+++ src/main/java/nofrills/features/hunting/CinderbatHighlight.java
@@ -2,7 +2,6 @@ package nofrills.features.hunting;
 
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.decoration.ArmorStandEntity;
 import net.minecraft.entity.passive.BatEntity;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
@@ -15,15 +14,12 @@ import nofrills.misc.EntityCache;
 import nofrills.misc.RenderColor;
 import nofrills.misc.Utils;
 
-import java.util.List;
-
 public class CinderbatHighlight {
     public static final Feature instance = new Feature("cinderbatHighlight");
 
     public static final SettingColor color = new SettingColor(RenderColor.fromHex(0x00ff00), "color", instance.key());
 
     private static final EntityCache cinderbatList = new EntityCache();
-    private static final EntityCache otherbatList = new EntityCache();
 
     @EventHandler
     private static void onRender(WorldRenderEvent event) {
@@ -39,29 +35,8 @@ public class CinderbatHighlight {
     private static void onUpdated(EntityUpdatedEvent event) {
         if (instance.isActive() && Utils.isInArea("Crimson Isle")) {
             if (event.entity instanceof BatEntity bat) {
-                List<Entity> armorstands = Utils.getOtherEntities(event.entity, 0.25, (e) -> {
-                    if (e instanceof ArmorStandEntity && e.getDisplayName() != null) {
-                        return e.getDisplayName().getString().contains("Primordial Bat");
-                    }
-                    return false;
-                });
-                if (!armorstands.isEmpty()) {
-                    cinderbatList.remove(event.entity);
-                    otherbatList.add(event.entity);
-                }
-                if (!otherbatList.has(event.entity) && bat.getHealth() > 20.0f && bat.getMaxHealth() == 6.0f && !cinderbatList.has(event.entity)) {
+                if (bat.getHealth() > 4800000.0f && bat.getMaxHealth() == 6.0f && !cinderbatList.has(event.entity)) {
                     cinderbatList.add(event.entity);
-                }
-            } else if (event.entity instanceof ArmorStandEntity) {
-                if (event.entity.getDisplayName() != null && event.entity.getDisplayName().getString().contains("Primordial Bat")) {
-                    Utils.getOtherEntities(event.entity, 0.25, (e) -> {
-                        if (e instanceof BatEntity) {
-                            cinderbatList.remove(e);
-                            otherbatList.add(e);
-                            return true;
-                        }
-                        return false;
-                    });
                 }
             }
         }
@@ -70,6 +45,5 @@ public class CinderbatHighlight {
     @EventHandler
     private static void onJoin(ServerJoinEvent event) {
         cinderbatList.clear();
-        otherbatList.clear();
     }
 }

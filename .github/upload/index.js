1c4f462deb84d4af5a9b40aa99e20191a3d8c05c
WhatYouThing <whatuthing@gmail.com>
2025-11-30 01:43:08 +0100

    remove top level async usage

--- .github/upload/index.js
+++ .github/upload/index.js
@@ -3,20 +3,17 @@ import { readdirSync, readFileSync } from "fs";
 const hash = process.argv[2];
 const commitMessage = process.argv[3];
 
-async function main() {
-    const file = readdirSync("../../build/libs", { withFileTypes: true }).at(0);
-    const res = await fetch(`https://whatyouth.ing/api/nofrills/v1/misc/post-beta-build`, {
-        method: "POST",
-        headers: {
-            "nf-auth": process.env.NF_API_BETA_AUTH,
-            "nf-beta-hash": hash,
-            "nf-beta-msg": commitMessage,
-            "nf-beta-ver": file.name.split("-").at(1)
-        },
-        body: readFileSync(`${file.parentPath}/${file.name}`)
-    });
-    console.log(`Build path: ${file.parentPath}/${file.name}`);
+const file = readdirSync("../../build/libs", { withFileTypes: true }).at(0);
+fetch(`https://whatyouth.ing/api/nofrills/v1/misc/post-beta-build`, {
+    method: "POST",
+    headers: {
+        "nf-auth": process.env.NF_API_BETA_AUTH,
+        "nf-beta-hash": hash,
+        "nf-beta-msg": commitMessage,
+        "nf-beta-ver": file.name.split("-").at(1)
+    },
+    body: readFileSync(`${file.parentPath}/${file.name}`)
+}).then(res => {
     console.log(`Response code: ${res.status}`);
-}
-
-await main();
+});
+console.log(`Build path: ${file.parentPath}/${file.name}`);

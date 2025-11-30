e05ddea877f0a759e7f5b5cc8fa3f1e5b9543d9b
WhatYouThing <whatuthing@gmail.com>
2025-11-30 03:07:14 +0100

    fix commit message not counting as single arg

--- .github/upload/index.js
+++ .github/upload/index.js
@@ -4,15 +4,17 @@ const hash = process.argv[2];
 const commitMessage = process.argv[3];
 
 const file = readdirSync("../../build/libs", { withFileTypes: true }).at(0);
-fetch(`https://whatyouth.ing/api/nofrills/v1/misc/post-beta-build`, {
+const bytes = readFileSync(`${file.parentPath}/${file.name}`);
+fetch("https://whatyouth.ing/api/nofrills/v1/misc/post-beta-build", {
     method: "POST",
     headers: {
-        "nf-auth": process.env.NF_API_BETA_AUTH,
+        "Content-Type": "application/octet-stream",
+        "nf-beta-auth": process.env.NF_API_BETA_AUTH,
         "nf-beta-hash": hash,
         "nf-beta-msg": commitMessage,
         "nf-beta-ver": file.name.split("-").at(1)
     },
-    body: readFileSync(`${file.parentPath}/${file.name}`)
+    body: bytes
 }).then(res => {
     console.log(`Response code: ${res.status}`);
 });

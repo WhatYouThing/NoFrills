214b3c9f6751cfeaba9bb2fbb5d1d941c0e06067
WhatYouThing <whatuthing@gmail.com>
2025-11-30 11:40:18 +0100

    upload build with json, 3rd times the charm

--- .github/upload/index.js
+++ .github/upload/index.js
@@ -1,21 +1,20 @@
 import * as fs from "fs";
-import * as path from "path";
 
-const file = fs.readdirSync("../../build/libs", { withFileTypes: true }).at(0);
+const file = fs.readdirSync(`${__dirname}/build/libs`, { withFileTypes: true }).at(0);
 const bytes = fs.readFileSync(`${file.parentPath}/${file.name}`);
-const form = new FormData();
-
-form.set("bytes", new Blob([bytes], { type: "application/java-archive" }), path.basename(`${file.parentPath}/${file.name}`));
-form.set("hash", process.argv[2]);
-form.set("version", file.name.split("-").at(1));
-form.set("message", process.argv[3]);
+const body = {
+    "bytes": bytes,
+    "hash": process.argv[2],
+    "version": file.name.split("-").at(1),
+    "message": process.argv[3]
+};
 
 fetch("https://whatyouth.ing/api/nofrills/v1/misc/post-beta-build", {
     method: "POST",
     headers: {
         "nf-beta-auth": process.env.NF_API_BETA_AUTH,
     },
-    body: form
+    body: JSON.stringify(body)
 }).then(res => {
     console.log(`Response code: ${res.status}`);
 });

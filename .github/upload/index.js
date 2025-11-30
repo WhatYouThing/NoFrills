a38fd006efb9a235afdd52b916263df81724fccf
WhatYouThing <whatuthing@gmail.com>
2025-11-30 13:32:01 +0100

    use async closure for upload

--- .github/upload/index.js
+++ .github/upload/index.js
@@ -3,19 +3,24 @@ import * as fs from "fs";
 const file = fs.readdirSync("../../build/libs", { withFileTypes: true }).at(0);
 const bytes = fs.readFileSync(`${file.parentPath}/${file.name}`);
 const body = {
-    "bytes": bytes,
-    "hash": process.argv[2],
-    "version": file.name.split("-").at(1),
-    "message": process.argv[3]
+  "bytes": bytes,
+  "hash": process.argv[2],
+  "version": file.name.split("-").at(1),
+  "message": process.argv[3]
 };
 
-fetch("https://whatyouth.ing/api/nofrills/v1/misc/post-beta-build", {
-    method: "POST",
-    headers: {
+(async () => {
+  try {
+    const res = await fetch("https://whatyouth.ing/api/nofrills/v1/misc/post-beta-build", {
+      method: "POST",
+      headers: {
         "nf-beta-auth": process.env.NF_API_BETA_AUTH,
-    },
-    body: JSON.stringify(body)
-}).then(res => {
+      },
+      body: JSON.stringify(body)
+    })
     console.log(`Response code: ${res.status}`);
-});
-console.log(`Build path: ${file.parentPath}/${file.name}`);
+    console.log(`Build path: ${file.parentPath}/${file.name}`);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+})();

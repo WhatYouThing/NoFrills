a28987a4f506e90b6088d33965e0de8da39510a6
WhatYouThing <whatuthing@gmail.com>
2026-01-18 12:11:16 +0100

    add mode setting to Tooltip Scale
    - add new dynamic mode which automatically scales down tooltips to make them always fit the screen
    - add custom mode which replicates the previous behavior

--- src/main/java/nofrills/features/misc/TooltipScale.java
+++ src/main/java/nofrills/features/misc/TooltipScale.java
@@ -2,9 +2,24 @@ package nofrills.features.misc;
 
 import nofrills.config.Feature;
 import nofrills.config.SettingDouble;
+import nofrills.config.SettingEnum;
 
 public class TooltipScale {
     public static final Feature instance = new Feature("tooltipScale");
 
+    public static final SettingEnum<ScaleMode> mode = new SettingEnum<>(ScaleMode.Dynamic, ScaleMode.class, "mode", instance);
     public static final SettingDouble scale = new SettingDouble(1.0, "scale", instance.key());
+
+    public static boolean isDynamic() {
+        return mode.value().equals(ScaleMode.Dynamic);
+    }
+
+    public static boolean isCustom() {
+        return mode.value().equals(ScaleMode.Custom);
+    }
+
+    public enum ScaleMode {
+        Dynamic,
+        Custom
+    }
 }
--- src/main/java/nofrills/hud/clickgui/ClickGui.java
+++ src/main/java/nofrills/hud/clickgui/ClickGui.java
@@ -294,7 +294,8 @@ public class ClickGui extends BaseOwoScreen<FlowLayout> {
                 )),
                 new Category("Misc", List.of(
                         new Module("Tooltip Scale", TooltipScale.instance, "Customize the scale of tooltips.", new Settings(List.of(
-                                new Settings.SliderDouble("Scale", 0.0, 4.0, 0.01, TooltipScale.scale, "The scale multiplier.")
+                                new Settings.Dropdown<>("Mode", TooltipScale.mode, "The scaling mode.\n\nDynamic: Automatically scales down tooltips so that they always fit the screen.\nCustom: Scales tooltips using the Custom Scale value."),
+                                new Settings.SliderDouble("Custom Scale", 0.0, 4.0, 0.01, TooltipScale.scale, "The custom scale multiplier. Ignored if using Dynamic mode.")
                         ))),
                         new Module("Recipe Lookup", RecipeLookup.instance, "Search up recipes for the hovered item with a keybind.", new Settings(List.of(
                                 new Settings.Keybind("Keybind", RecipeLookup.keybind, "The key that activates the feature.")

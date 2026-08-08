2741050feb1ee2b23a41ee9b09e4bf63d0856d3c
WhatYouThing <whatuthing@gmail.com>
2026-08-08 20:11:56 +0200

    fix Lasso Alert misfiring if another player is hunting nearby
    - adds reel title alongside sound effect

--- src/main/java/nofrills/features/hunting/LassoAlert.java
+++ src/main/java/nofrills/features/hunting/LassoAlert.java
@@ -1,12 +1,20 @@
 package nofrills.features.hunting;
 
 import meteordevelopment.orbit.EventHandler;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.world.item.Items;
+import net.minecraft.world.entity.Leashable;
+import net.minecraft.world.entity.LivingEntity;
 import nofrills.config.Feature;
 import nofrills.events.EntityNamedEvent;
 import nofrills.events.EventListener;
+import nofrills.hud.HudManager;
 import nofrills.misc.Utils;
+
+import java.util.List;
+
+import static nofrills.Main.mc;
 
 @EventListener
 public class LassoAlert {
@@ -14,8 +22,16 @@ public class LassoAlert {
 
     @EventHandler
     private static void onNamed(EntityNamedEvent event) {
-        if (instance.isActive() && event.namePlain.equals("REEL") && Utils.getHeldItem().getItem().equals(Items.LEAD)) {
-            Utils.playSound(SoundEvents.NOTE_BLOCK_PLING, 1.0f, 1.0f);
+        if (instance.isActive() && mc.player != null && event.namePlain.equals("REEL")) {
+            List<Leashable> leashed = Leashable.leashableLeashedTo(mc.player);
+            if (leashed.isEmpty()) return;
+            for (Leashable leashable : leashed) {
+                if (leashable instanceof LivingEntity living && Utils.horizontalDistance(event.entity, living) <= 2.0) {
+                    HudManager.setCustomTitle(Component.literal("REEL").withStyle(ChatFormatting.YELLOW), 20);
+                    Utils.playSound(SoundEvents.NOTE_BLOCK_PLING, 1.0f, 1.0f);
+                    break;
+                }
+            }
         }
     }
 }
